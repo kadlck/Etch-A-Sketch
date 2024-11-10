9bac9640ac72788ccab4c6921db11dd04ffd655f
Dominika <dom.kadleckova@gmail.com>
2024-11-10 12:44:35 +0100

    Added shading function

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function createGrid(height, width){
        block.classList.add("inside", i);
        block.style.width = computeSize(width) + "px";
        block.style.height = computeSize(height) + "px";
-       block.style.backgroundColor = "grey";
+       block.style.backgroundColor = "rgb(150, 150, 150)";
        block.addEventListener("mouseover", changecolor)
        gridBase.appendChild(block);
     }
@@ -20,19 +20,41 @@ function computeSize(number){
 }
 
 function changecolor(event){
-    blockClass = event.currentTarget.getAttribute("class")
-    const blockIwant = document.getElementsByClassName(blockClass)[0];
-    blockIwant.style.backgroundColor = color;
+    if (shading=="ON"){
+        blockClass = event.currentTarget.getAttribute("class")
+        const blockIwant = document.getElementsByClassName(blockClass)[0];
+        oldColor = blockIwant.style.backgroundColor
+        oldColor = oldColor.replace("rgb(", "")
+        oldColor = oldColor.replace(")", "")
+        oldColor = oldColor.split(",")
+        if (oldColor.length == 3){
+            let r = oldColor[0]
+            let g = oldColor[1]
+            let b = oldColor[2]
+            let percentage= 0.25
+            const shadeR = Math.round(Math.max(0, r - r * percentage)); // 92
+            const shadeG = Math.round(Math.max(0, g - g * percentage)); // 46
+            const shadeB = Math.round(Math.max(0, b - b * percentage)); // 138
+            color = "rgb("+shadeR+","+shadeG+","+shadeB+")"
+            console.log(color)
+        }
+        blockIwant.style.backgroundColor = color; 
+    }else{
+        blockClass = event.currentTarget.getAttribute("class")
+        const blockIwant = document.getElementsByClassName(blockClass)[0];
+        blockIwant.style.backgroundColor = color; 
+    }
+    
 }
 
 
 let height = 16
 let width = 16
 let color = "black"
-
-function setUpButtons(){
     let shading = "OFF"
     let rainbow = "OFF"
+
+function setUpButtons(){
     const command = document.querySelector(".command")
     const CreateButton = document.createElement("button");
     CreateButton.classList.add("button", "create");
@@ -50,10 +72,20 @@ function setUpButtons(){
     });
     const ShadeButton = document.createElement("button");
     ShadeButton.classList.add("button", "shade");
-    ShadeButton.textContent = "I shade!"
-    ShadeButton.addEventListener("click", function colorDarkeing(
-        ////
-    ){})
+    ShadeButton.textContent = "Shading: "+shading
+    ShadeButton.style.backgroundColor= "rgb(233, 233, 233)"
+    ShadeButton.addEventListener("click", function changeVariable(){
+        if (shading=="ON"){
+            shading = "OFF";
+            ShadeButton.style.backgroundColor = "rgb(233, 233, 233)";
+            ShadeButton.textContent = "Shading: "+shading     
+        }
+        else{
+            shading = "ON";
+            ShadeButton.style.backgroundColor = "brown";
+            ShadeButton.textContent = "Shading: "+shading
+        }
+    })
     const SpaceButton = document.createElement("button");
     SpaceButton.classList.add("button", "space");
 
